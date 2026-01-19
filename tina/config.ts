9631c6c16da093c39774743b943acd38fb636a7b
Guz <5664957+guzmanlopez@users.noreply.github.com>
2026-01-19 22:48:42 +0000

    update deploy actions to latest versions and set default branch to main

--- tina/config.ts
+++ tina/config.ts
@@ -6,7 +6,7 @@ const branch =
   process.env.GITHUB_BRANCH ||
   process.env.VERCEL_GIT_COMMIT_REF ||
   process.env.HEAD ||
-  "feature-tina-website";
+  "main";
 
 // ============================================================================
 // SHARED FIELD DEFINITIONS
